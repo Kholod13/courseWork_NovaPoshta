b8d9d2759239f670cf6030f879e44ddb41c785f4
Kholod13 <111706893+Kholod13@users.noreply.github.com>
2023-02-21 01:27:17 +0200

    changed database, create must structures and create musst files

--- courseWork_NovaPoshta/classes.cpp
+++ courseWork_NovaPoshta/classes.cpp
@@ -1,149 +1,70 @@
 #include <iostream>
+#include <vector>
+#include <fstream>
 using std::cout;
 using std::cin;
 using std::string;
 using std::endl;
-using std::exception;
-
-class List
-{
-private:
-	struct Node
-	{
-		long data;
-		Node* next;
-	};
-
-	// head of the list
-	Node* head;
-	// tail of the list
-	Node* tail;
-	// size of the list
-	int size;
-
-public:
-	List() : head(nullptr), tail(nullptr), size(0) { }
-	~List()
-	{
-		Clear();
-	}
-
-	bool IsEmpty() const { return size == 0; }
-
-	void AddHead(long data)
-	{
-		// create new item
-		Node* item = new Node;
-		item->data = data;	// set data
-		item->next = head;	// link with next item
-
-		// set tail if the list is empty
-		if (IsEmpty()) tail = item;
-
-		// set new item as a head
-		head = item;
-
-		// increase size
-		++size;
-	}
-	void DeleteHead()
-	{
-		if (IsEmpty()) return;
-
-		// save current head
-		Node* temp = head;
-
-		// set new head
-		head = head->next;
-
-		// delete previous head item
-		delete temp;
-
-		// decrease size
-		--size;
-
-		// clear tail the list is empty
-		if (IsEmpty()) tail = nullptr;
-	}
-
-	void AddTail(long data)
-	{
-		// create new item
-		Node* item = new Node;
-		item->data = data;
-		item->next = nullptr;
-
-		if (IsEmpty())
-			head = item; // set head if the list is empty
-		else
-			tail->next = item; // link new item with the previous
-
-		// set new item as a tail
-		tail = item;
-
-		// increase size
-		++size;
-	}
-
-	long GetFirst() const
-	{
-		if (IsEmpty()) throw exception("List is empty.");
-
-		return head->data;
-	}
-	long GetLast() const
-	{
-		if (IsEmpty()) throw exception("List is empty.");
-
-		return tail->data;
-	}
-	int GetCount() const
-	{
-		return size;
-	}
-
-	void Show() const
-	{
-		// using while cycle
-		/*Node* item = head;
-		while (item != nullptr)
-		{
-			cout << item->data << " ";
-			item = item->next;
-		}*/
-
-		// using for
-		for (Node* i = head; i != nullptr; i = i->next)
-		{
-			cout << i->data << " ";
-		}
-		cout << endl;
-	}
-
-	void Clear()
-	{
-		Node* item = head;
-		while (item != nullptr)
-		{
-			Node* next = item->next;
-			delete item;
-			item = next;
-		}
-		head = tail = nullptr;
-		size = 0;
-	}
-};
+using std::vector;
+using std::fstream;
 
 struct Product {
 	string name;
+	string company;
 	int price;
-	string receipt;
+	int guarantee;
+};
+
+/*
+struct Receipt {
+	string nameProduct;
+	string priceProduct;
+	int year;
+	int month;
+	int day;
+};
+*/
+
+struct StatusService {
+	string none = "none!";
+	string inProcess = "become...";
+	string done = "done!";
+};
+
+struct Database {
+	Product product;
+	int year;
+	int month;
+	int day;
+	string status;
 };
 
 class Mail {
 private:
-	List list;
+	vector<Product>listProducts;
+	vector<Database>database;
+
+	int balance;
+	
+	string nameNS = "New Services";
+	fstream newServices;
+	string nameDS = "Done Services";
+	fstream doneServices;
+
+	StatusService status;
 
 public:
+	Mail(int balance) : balance(balance)
+	{
+		newServices.open(nameNS);
+		doneServices.open(nameDS);
+	}
+	~Mail()
+	{
+		newServices.close();
+		doneServices.close();
+	}
+
+
 
 };
